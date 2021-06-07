4ccae4ec66323acdf8423551e3ce85576606fd86
Elena-Weber <crochetmiracles@gmail.com>
2021-06-07 15:35:24 -0400

    commit tree abdce98d0556f7b8257c15171fd28c0aa10c4375

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Your code here
+function saturdayFun(word="roller-skate") {
+    return(`This Saturday, I want to ${word}!`)
+}
+function mondayWork(word="go to the office") {
+    return(`This Monday, I will ${word}.`)
+}
+function wrapAdjective(ast="*") {
+    return function(word="special") {
+        return(`You are ${ast}${word}${ast}!`)
+    }
+}
+let Calculator = {
+    add: function(x, y) {
+        return x + y;
+    },
+    subtract: function(x, y) {
+        return x - y;
+    },
+    multiply: function(x, y) {
+        return x * y;
+    },
+    divide: function(x, y) {
+        return x / y;
+    }
+}
+function actionApplyer(num, arr) {
+    for (let i = 0; i < arr.length; i++ ){
+        num = arr[i](num)
+    }
+    return num
+}
